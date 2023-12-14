4a56d64935e5e3f020264a45bffbc4fb298e8c4d
zhaoxinyuan <1178232101@qq.com>
2023-12-14 19:15:17 +0800

    坐标值改成2个浮点数字段了，可以用earth_distance函数计算距离，但是应该where范围画圆应该是gg了

--- app/flag/typedef.py
+++ app/flag/typedef.py
@@ -1,20 +1,50 @@
 from datetime import datetime
-from typing import Optional, Tuple
+from typing import Optional, Tuple, List
 from app.util import Model
+from typedef import Order
 
 
 class Flag(Model):
     id: Optional[int]
     user_id: Optional[int]
-    location: Optional[Tuple[float, float]]
+    location_x: Optional[float]
+    location_y: Optional[float]
     content: Optional[str]
+    type: Optional[int]
     is_open: Optional[int]
     create_time: Optional[datetime]
     update_time: Optional[datetime]
-    has_picture: Optional[int]
+    pictures: Optional[List[str]]
+
+
+class Comment(Model):
+    id: Optional[int]
+    flag_id: Optional[int]
+    user_id: Optional[int]
+    content: Optional[str]
+    root_comment_id: Optional[int]
+    location_x: Optional[float]
+    location_y: Optional[float]
+    prefix: Optional[str]
+    comment_time: Optional[datetime]
 
 
 class AddFlag(Flag):
+    user_id: int
     location: Tuple[float, float]
     content: str
+
     is_open: int
+    pictures: List[str]
+
+
+class _GetFlag(Flag):
+    by: str
+
+
+class GetFlagById(Order, _GetFlag):
+    id: int
+
+
+class GetFlagByLocation(_GetFlag):
+    location: Tuple[float, float]
