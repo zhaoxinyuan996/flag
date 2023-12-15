4636c2596bd05a1cf4d9971375e9238651c824c6
zhaoxinyuan <1178232101@qq.com>
2023-12-15 19:06:04 +0800

    部署下

--- main.py
+++ main.py
@@ -7,4 +7,6 @@ from util.config import dev
 if __name__ == '__main__':
     if not dev:
         threading.Thread(target=DelayJob.run).start()
+        app.run(host='0.0.0.0', port=8999)
+
     app.run()
