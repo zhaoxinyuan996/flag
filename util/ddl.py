4191b598fa87aa942b4ceae79096ef3d422913ac
zhaoxinyuan <1178232101@qq.com>
2023-12-22 11:25:53 +0800

    表设计，不包含sql改动

--- util/ddl.py
+++ util/ddl.py
@@ -5,17 +5,15 @@ create table test (f1 int, f2 text, f3 int[], f4 text[], f5 json);
 # 用户表
 d2 = '''
 create table users (
-id serial primary key, 
+id uuid primary key, 
 
 nickname text not null,
-username text, 
-password text,
+username text not null, 
+password text not null,
 phone int,
-sex int,
+is_man bool,
 
 wechat_id text,
-google_id text,
-apple_id text,
 
 signature text,
 profile_picture text,
@@ -26,21 +24,24 @@ vip_deadline timestamp,
 block_deadline timestamp,
 alive_deadline timestamp,
 belong text,
-location_x numeric,
-location_y numeric,
+location geometry,
+extend1 text,
+extend2 text,
+extend3 text,
 UNIQUE(username),
-UNIQUE(wechat_id),
-UNIQUE(google_id),
-UNIQUE(apple_id)
+UNIQUE(wechat_id)
 );
 '''
 
 # 关注表
 d3 = '''
 create table follow(
-fans_id int not null,
-star_id int not null,
-primary key(fans_id, star_id)
+fans_id uuid not null,
+star_id uuid not null,
+primary key(fans_id, star_id),
+extend1 text,
+extend2 text,
+extend3 text
 );
 
 CREATE INDEX fans_index ON follow(fans_id);
@@ -50,45 +51,51 @@ CREATE INDEX star_index ON follow(star_id);
 # 注销表
 d4 = '''
 create table sign_out_users (
-user_id int primary key,
-out_time timestamp not null
+user_id uuid primary key,
+out_time timestamp not null,
+extend1 text,
+extend2 text,
+extend3 text
 )
 '''
 
 # flag表
 d5 = '''
 create table flag (
-id serial primary key,
+id uuid primary key,
 user_id int not null,
-location_x numeric not null,
-location_y numeric not null,
+location geometry not null,
 content text,
 type int,
 is_open int not null,
 create_time timestamp not null,
 update_time timestamp not null,
-pictures text[] not null
+pictures text[] not null,
+extend1 text,
+extend2 text,
+extend3 text
 );
 
 CREATE INDEX type_index ON flag(type);
 CREATE INDEX is_open_index ON flag(is_open);
 CREATE INDEX user_id_index ON flag(user_id);
-CREATE INDEX flag_x_index ON flag(location_x);
-CREATE INDEX flag_y_index ON flag(location_y);
+CREATE INDEX flag_location_index ON flag(location);
 '''
 
 # 评论表
 d6 = '''
 create table flag_comment(
 id serial primary key,
-flag_id int not null,
-user_id int not null,
+flag_id uuid not null,
+user_id uuid not null,
 content text not null,
 root_comment_id int,
-location_x numeric not null,
-location_y numeric not null,
+distance int not null,
 prefix text,
-comment_time timestamp not null
+comment_time timestamp not null,
+extend1 text,
+extend2 text,
+extend3 text
 );
 
 CREATE INDEX flag_comment_index ON flag_comment(flag_id);
@@ -98,10 +105,13 @@ CREATE INDEX root_comment_index ON flag_comment(root_comment_id);
 # 黑名单
 d7 = '''
 create table black_list(
-user_id int not null,
-black_id int not null,
+user_id uuid not null,
+black_id uuid not null,
 update_time timestamp,
-primary key(user_id, black_id)
+primary key(user_id, black_id),
+extend1 text,
+extend2 text,
+extend3 text
 );
 
 CREATE INDEX user_black_index ON black_list(user_id);
@@ -115,8 +125,16 @@ type int not null,
 send_id int not null,
 receive_id int not null,
 content text not null,
-create_time timestamp not null
+create_time timestamp not null,
+extend1 text,
+extend2 text,
+extend3 text
 );
 
 CREATE INDEX receive_id_message_index ON message(receive_id);
+'''
+
+# 收藏表
+d9 = '''
+
 '''
