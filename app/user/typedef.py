c64f61f53625f1ab65bb4698c9eb631ffea0c898
zhaoxinyuan <1178232101@qq.com>
2024-01-21 20:59:29 +0800

    参数校验小优化

--- app/user/typedef.py
+++ app/user/typedef.py
@@ -11,27 +11,8 @@ _NICKNAME = constr(max_length=20)
 _SIGNATURE = constr(max_length=50)
 
 
-class User(Model):
-    id: UUID
-
-    nickname: _NICKNAME
-    username: Optional[str]
-    password: Optional[str]
-    phone: Optional[int]
-    is_man: Optional[bool]
-    signature: Optional[_SIGNATURE]
-    avatar_name: Optional[str]
-    bg_avatar_name: Optional[str]
-    flag_num: int
-    create_time: datetime
-    vip_deadline: Optional[datetime]
-    block_deadline: Optional[datetime]
-    alive_deadline: Optional[datetime]
-
-    belong: Optional[str]
-    local: Optional[str]
-    hidden: bool
-
+class UserMixin:
+    """抽象Mixin类"""
     @property
     def user_class(self) -> int:
         if self.block_deadline and self.block_deadline > datetime.now():
@@ -54,6 +35,43 @@ class User(Model):
         elif self.user_class == UserClass.hidden:
             return FlagNum.hidden_user - self.flag_num
         raise UndefinedError('user class')
+
+
+class User(Model, UserMixin):
+    id: UUID
+
+    nickname: _NICKNAME
+    username: Optional[str]
+    password: Optional[str]
+    phone: Optional[int]
+    is_man: Optional[bool]
+    signature: Optional[_SIGNATURE]
+    avatar_name: str
+    bg_avatar_name: Optional[str]
+    flag_num: int
+    create_time: datetime
+    vip_deadline: datetime
+    block_deadline: datetime
+    alive_deadline: datetime
+
+    belong: Optional[str]
+    local: Optional[str]
+    hidden: bool
+
+
+class SelfUser(Model, UserMixin):
+    id: UUID
+
+    nickname: _NICKNAME
+    username: Optional[str]
+    phone: Optional[int]
+    is_man: Optional[bool]
+    signature: Optional[_SIGNATURE]
+    avatar_name: str
+    bg_avatar_name: Optional[str]
+    flag_num: int
+    vip_deadline: datetime
+    block_deadline: datetime
 
 
 class QueryUser(Model):
