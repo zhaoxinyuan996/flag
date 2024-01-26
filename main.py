e121501838faf03c286464a002f5489f9cfbfe13
zhaoxinyuan <1178232101@qq.com>
2024-01-26 14:57:06 +0800

    异步获取ip地理位置改用mq

--- main.py
+++ main.py
@@ -1,14 +1,7 @@
-import threading
 from util.config import dev
 from app import app
-from common.job import DelayJob
 
 
-if not dev:
-    threading.Thread(target=DelayJob.run).start()
-
-
-if __name__ == '__main__':
-    if dev:
-        print('dev')
-        app.run()
+if dev:
+    print('dev')
+    app.run()
